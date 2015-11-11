a3d8709ff9d5c51e5719c3584d01c56c15148676
Michael Aaron Murphy <mmstickman@gmail.com>
2015-11-11 16:12:18 -0600

    Add gtk::Separators to Each Unit

--- src/systemctl_gui.rs
+++ src/systemctl_gui.rs
@@ -18,22 +18,37 @@ pub fn create_main_window() -> gtk::Window {
 
 pub fn generate_services() -> gtk::ScrolledWindow {
 	let scrolled_window = gtk::ScrolledWindow::new(None, None).unwrap();
-	let service_list = gtk::Box::new(gtk::Orientation::Vertical, 0).unwrap();
-	let socket_list = gtk::Box::new(gtk::Orientation::Vertical, 0).unwrap();
 
 	let mut label_font = pango::FontDescription::new();
 	label_font.set_weight(pango::Weight::Heavy);
-	let service_label = gtk::Label::new("Services (Activate on Startup)").unwrap();
-	let socket_label = gtk::Label::new("Sockets (Activate On Use)").unwrap();
-	service_label.override_font(&label_font);
-	service_list.pack_start(&service_label, true, true, 0);
-	socket_label.override_font(&label_font);
-	socket_list.pack_start(&socket_label, true, true, 0);
+
+	let service_list = {
+		let list = gtk::Box::new(gtk::Orientation::Vertical, 0).unwrap();
+		let service_label = gtk::Label::new("Services (Activate on Startup)").unwrap();
+		service_label.override_font(&label_font);
+		list.pack_start(&service_label, true, true, 0);
+		list
+	};
+
+	let socket_list = {
+		let list = gtk::Box::new(gtk::Orientation::Vertical, 0).unwrap();
+		list.add(&gtk::Separator::new(gtk::Orientation::Horizontal).unwrap());
+		let socket_label = gtk::Label::new("Sockets (Activate On Use)").unwrap();
+		socket_label.override_font(&label_font);
+		list.pack_start(&socket_label, true, true, 0);
+		list
+	};
 
 	for unit in systemctl::get_unit_files() {
 		match unit.unit_type {
-			systemctl::UnitType::Service => service_list.pack_start(&get_unit(unit), false, false, 3),
-			systemctl::UnitType::Socket => socket_list.pack_start(&get_unit(unit), false, false, 3),
+			systemctl::UnitType::Service => {
+					service_list.add(&gtk::Separator::new(gtk::Orientation::Horizontal).unwrap());
+					service_list.pack_start(&get_unit(unit), false, false, 3);
+			},
+			systemctl::UnitType::Socket => {
+				socket_list.add(&gtk::Separator::new(gtk::Orientation::Horizontal).unwrap());
+				socket_list.pack_start(&get_unit(unit), false, false, 3);
+			},
 		};
 	}
 
@@ -55,7 +70,7 @@ fn get_unit(unit: systemctl::SystemdUnit) -> gtk::Box {
 	{ // Defines action when clicking the button. Consider this to be it's own thread.
 		let service_name: String = match unit.unit_type {
 			systemctl::UnitType::Socket => unit.name.clone(),
-			systemctl::UnitType::Service => unit.name.chars().take_while(|x| *x != '.').collect(),
+			systemctl::UnitType::Service => unit.name.chars().take(unit.name.len()-8).collect(),
 		};
 		switch.connect_clicked(move |switch| {
 			if &switch.get_label().unwrap() == "Disable" {
@@ -69,5 +84,6 @@ fn get_unit(unit: systemctl::SystemdUnit) -> gtk::Box {
 	let layout = gtk::Box::new(gtk::Orientation::Horizontal, 0).unwrap();
 	layout.pack_start(&label, false, false, 15);
 	layout.pack_start(&switch, true, true, 15);
+
 	return layout;
 }
