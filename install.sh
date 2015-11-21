5887ee051e57e201db45c1d314d0aa287c40798c
Michael Aaron Murphy <mmstickman@gmail.com>
2015-11-20 23:26:53 -0600

    Enable Policykit Permissions

--- install.sh
+++ install.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 cargo build --release
-sudo install target/release/systemd-manager /usr/local/bin/
+sudo cp target/release/systemd-manager /usr/local/bin/
+sudo cp systemd-manager-pkexec /usr/local/bin
+sudo chmod +x /usr/local/bin/systemd-manager-pkexec
+sudo chmod +x /usr/local/bin/systemd-manager
 sudo cp systemd-manager.desktop /usr/share/applications/
+sudo cp org.freedesktop.policykit.systemd-manager.policy /usr/share/polkit-1/actions/
